7e0fbd2a5c78ca705caff5312b6b3a32ffab5561
TereFranco <112712182+TereFranco@users.noreply.github.com>
2025-04-05 10:07:01 +0200

    Created environment and guion APIREST

---
+++ P3-Django/myApiRest/auctions/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Auction
+from .serializers import CategoryListCreateSerializer, CategoryDetailSerializer, AuctionListCreateSerializer, AuctionDetailSerializer
+
+# Create your views here.
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListCreateSerializer
+
+
+class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryDetailSerializer
+
+
+class AuctionListCreate(generics.ListCreateAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionListCreateSerializer
+
+
+class AuctionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Auction.objects.all()
+    serializer_class = AuctionDetailSerializer
