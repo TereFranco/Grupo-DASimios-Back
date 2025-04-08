5a07381f093b8c57c2f9c55a439bee020319311d
ULISESDLAPTOP\ulise <202211403@alu.comillas.edu>
2025-04-08 17:18:08 +0200

    Users Guide Added

--- P3-Django/myApiRest/myApiRest/settings.py
+++ P3-Django/myApiRest/myApiRest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -30,17 +31,20 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS = [ 
+    'django.contrib.admin', 
+    'django.contrib.auth', 
+    'django.contrib.contenttypes', 
+    'django.contrib.sessions', 
+    'django.contrib.messages', 
+    'django.contrib.staticfiles', 
+    'rest_framework',  
+    'rest_framework_simplejwt', 
+    'rest_framework_simplejwt.token_blacklist', 
     'auctions',
-    'rest_framework',
     'drf_spectacular',
     'corsheaders',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -131,6 +135,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': ( 
+        'rest_framework_simplejwt.authentication.JWTAuthentication', 
+        ), 
 }
 
 SPECTACULAR_SETTINGS = {
@@ -142,3 +149,12 @@ SPECTACULAR_SETTINGS = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
+
+SIMPLE_JWT = { 
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1), 
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7), 
+    "ROTATE_REFRESH_TOKENS": True, 
+    "BLACKLIST_AFTER_ROTATION": True,  
+    } 
+
+AUTH_USER_MODEL = 'users.CustomUser' 
--- P3-Django/myApiRest/myApiRest/urls.py
+++ P3-Django/myApiRest/myApiRest/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from django.contrib import admin 
+from django.urls import include, path 
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView 
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView) 
 
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/auctions/', include('auctions.urls')),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'),
-    name='swagger-ui')
-]
+urlpatterns = [ 
+    path("api/auctions/", include("auctions.urls")), 
+    path("api/users/", include("users.urls")), 
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path("admin/", admin.site.urls), 
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'), 
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), 
+    name='swagger-ui'), 
+    ] 
