b8f4ff738aac9840da1144e22ece9d682a74faad
TereFranco <112712182+TereFranco@users.noreply.github.com>
2025-04-13 20:27:10 +0200

    allowed host on render

--- P3-Django/myApiRest/myApiRest/settings.py
+++ P3-Django/myApiRest/myApiRest/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-vti%yma6w90y_&2-!qp_#a58=nk)n7xc30q-ncq1_7zgo%*c-7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '@dpg-cvtv207gi27c73abi190-a.oregon-postgres.render.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '@dpg-cvtv207gi27c73abi190-a.oregon-postgres.render.com', 'grupo-dasimios-back.onrender.com']
 
 
 # Application definition
